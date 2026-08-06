2da722074edeca63dce30d00c27aafa9a5444e24
Tanish Poddar <tanish.iaf1852005@gmail.com>
2026-08-06 09:49:44 +0530

    Optimized Project Section

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -76,7 +76,6 @@ export default async function ProjectsPage() {
                       fill
                       className="object-cover"
                       sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 33vw"
-                      unoptimized
                     />
                   </div>
 
